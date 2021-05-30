d1cccaf4fb1e510d47380df52a56a81cc720cd70
Alexander Lavrov <jiabp.lad@yandex.com>
2021-05-30 21:28:51 +0500

    Add StructuralChars

---
+++ StructuralChars.cs
@@ -0,0 +1,12 @@
+﻿namespace Parser
+{
+    public class StructuralChars
+    {
+        public const char BeginArray = '[';
+        public const char EndArray = ']';
+        public const char BeginObject = '{';
+        public const char EndObject = '}';
+        public const char NameSeparator = ':';
+        public const char ValueSeparator = ',';
+    }
+}
